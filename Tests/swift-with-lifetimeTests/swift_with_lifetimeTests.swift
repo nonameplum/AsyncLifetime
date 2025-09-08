eded7dfaafbf3998ff4a473d8fd8cc504b3e586d
Łukasz Śliwiński <sliwinski.lukas@gmail.com>
2025-09-08 22:28:03 +0200

    Initial Commit

---
+++ Tests/swift-with-lifetimeTests/swift_with_lifetimeTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import swift_with_lifetime
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
